a53d1f3e94e05a90cf1803fc4e1740b18918e27b
sidereus <francesco.serafin.3@gmail.com>
2015-12-14 16:55:35 +0100

    Added license to OMSComponent.java

--- src/oms_bmi/OMSComponent.java
+++ src/oms_bmi/OMSComponent.java
@@ -1,3 +1,21 @@
+/*
+ * $Id$
+ *
+ * This file is part of the Object Modeling System (OMS),
+ * 2007-2012, Olaf David and others, Colorado State University.
+ *
+ * OMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 2.1.
+ *
+ * OMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OMS.  If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+ */
 package oms_bmi;
 
 import bmi.BMI;
@@ -5,6 +23,11 @@ import oms3.annotations.Execute;
 import oms3.annotations.Finalize;
 import oms3.annotations.Initialize;
 
+/**
+ * OMS wrapper for a BMI component.
+ *
+ * @author od
+ */
 public class OMSComponent {
 
     public BMI comp;
@@ -35,4 +58,5 @@ public class OMSComponent {
     public void done() throws Exception {
         comp.finalize();
     }
+
 }
