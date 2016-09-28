00fb79b6bc3ab9427e8cb88d04d9a8d57b87a080
sidereus <francesco.serafin.3@gmail.com>
2016-09-28 16:34:33 +0200

    Added Copyright to JUnit tests classes

--- src/test/java/tw/Hamon_BMI.java
+++ src/test/java/tw/Hamon_BMI.java
@@ -1,3 +1,21 @@
+/*
+ * $Id$
+ *
+ * This file is part of the Object Modeling System (OMS),
+ * 2007-2016, Olaf David and others, Colorado State University.
+ *
+ * OMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 2.1.
+ *
+ * OMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OMS.  If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+ */
 package tw;
 
 import java.io.FileInputStream;
